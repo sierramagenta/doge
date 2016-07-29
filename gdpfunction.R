5df71ebaf1c3eb1f88a7557f101473e57d38be4f
sierramagenta <lovestowell@gmail.com>
2016-07-29 15:01:42 -0600

    sierra added gdp func

---
+++ gdpfunction.R
@@ -0,0 +1,13 @@
+#a function to calculate GDP from the Gapminder Dataset
+calcGDP <- function(dat, year=NULL, country=NULL) {
+  if (!is.null(year)) {
+    dat <- dat[dat$year %in% year, ]
+  }
+  if (!is.null(country)) {
+    dat <- dat[dat$country %in% country,]
+  }
+  gdp <- dat$pop * dat$gdpPercap
+  
+  new <- cbind(dat, gdp=gdp)
+  return(new)
+}
